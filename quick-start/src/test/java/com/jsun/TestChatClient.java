43229768a5775cf9550dddc6fef3e7d73b7024fb
jsun <sjh_jy@163.com>
2025-09-25 11:29:48 +0800

    - 接入阿里百炼-未注册 API Key - Chat Client 实践

---
+++ quick-start/src/test/java/com/jsun/TestChatClient.java
@@ -0,0 +1,41 @@
+package com.jsun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+
+@SpringBootTest
+public class TestChatClient {
+    @Test
+    public void testChatClient(@Autowired ChatClient.Builder checkClientBuilder) {
+        ChatClient chatClient = checkClientBuilder.build();
+        String content = chatClient.prompt()
+                .user("Hello")
+                .call()
+                .content();
+        System.out.println(content);
+    }
+
+    @Test
+    public void testChatClientStream(@Autowired ChatClient.Builder checkClientBuilder) {
+        ChatClient chatClient = checkClientBuilder.build();
+        Flux<String> content = chatClient.prompt()
+                .user("What's the weather today in Shanghai China.")
+                .stream()
+                .content();
+        content.toIterable().forEach(System.out::print);
+    }
+
+    @Test
+    public void testMultiChatClients(@Autowired DeepSeekChatModel deepSeekChatModel) {
+        ChatClient chatClient = ChatClient.builder(deepSeekChatModel).build();
+        String content = chatClient.prompt()
+                .user("Good morning")
+                .call()
+                .content();
+        System.out.println(content);
+    }
+}
